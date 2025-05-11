634d87590deb04e85e9981f2bfc5aade623f2443
wangguangwu <wangguangwu@pukanghealth.com>
2025-05-11 12:12:27 +0800

    first commit

---
+++ src/main/java/com/wangguangwu/cthulhurpc/CthulhuRpcApplication.java
@@ -0,0 +1,13 @@
+package com.wangguangwu.cthulhurpc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CthulhuRpcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CthulhuRpcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wangguangwu/cthulhurpc/CthulhuRpcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wangguangwu.cthulhurpc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CthulhuRpcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
